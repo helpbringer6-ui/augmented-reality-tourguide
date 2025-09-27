684b229cbb873a3e879d639398a0998f4bbc1154
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-27 07:21:54 -0500

    Update welcome message for clarity and appeal.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Welcome to your immersive Augmented Reality Tour Guide!</Text>
+      <Text>Welcome to your immersive Augmented Reality Tour Experience!</Text>
     </SafeAreaView>
   );
 };
