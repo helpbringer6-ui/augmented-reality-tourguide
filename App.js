1859567d45fa0b084507ec4f70697fda8a7457a6
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-27 07:22:55 -0500

    Update welcome message for clarity.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Welcome to your immersive Augmented Reality Tour Experience!</Text>
+      <Text>Welcome to your immersive Augmented Reality Tour Experience! Enjoy your journey!</Text>
     </SafeAreaView>
   );
 };
